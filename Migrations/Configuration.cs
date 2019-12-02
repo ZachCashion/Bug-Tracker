0087047805a708014a0d96bf8e5cd120e7712b40
ZachCashion <zach.cashion@gmail.com>
2019-12-01 23:22:44 -0500

    Update

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -8,9 +8,14 @@ namespace Bug_Tracker.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using System.Web.Configuration;
+    using Bug_Tracker.Helpers;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Bug_Tracker.Models.ApplicationDbContext>
     {
+        private UserRolesHelper rolesHelper = new UserRolesHelper();
+        private ProjectsHelper projectsHelper = new ProjectsHelper();
+        private Random random = new Random();
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
@@ -140,20 +145,78 @@ namespace Bug_Tracker.Migrations
                     new TicketStatus { Name = "Archived", Description = "" }
                 );
 
-            //Preload Data
-            //context.Projects.AddOrUpdate(
-               // new Project {
+            //Projects
+            context.Projects.AddOrUpdate(
+                p => p.Name,
+                    new Project() { Name = "First Demo", Description = "The first demo project seed", Created = DateTime.Now.AddDays(-3) },
+                    new Project() { Name = "Second Demo", Description = "The second demo project seed", Created = DateTime.Now.AddDays(-7) },
+                    new Project() { Name = "Third Demo", Description = "The third demo project seed", Created = DateTime.Now.AddDays(-15) },
+                    new Project() { Name = "Fourth Demo", Description = "The fourth demo project seed", Created = DateTime.Now.AddDays(-45) },
+                    new Project() { Name = "Fifth Demo", Description = "The fifth demo project seed", Created = DateTime.Now.AddDays(-60) } 
+                );
 
-               // }
+            context.SaveChanges();
 
-               // );
+            var projects = context.Projects.ToList();
+            var statuses = context.TicketStatus.ToList();
+            var priorities = context.TicketPriorities.ToList();
+            var types = context.TicketTypes.ToList();
+            var users = context.Users;
+            var roles = context.Roles;
+            var maxRandomSubmitter = rolesHelper.UsersInRole("Submitter").ToList().Count();
+            var maxRandomDeveloper = rolesHelper.UsersInRole("Developer").ToList().Count();
 
-           // context.Tickets.AddOrUpdate(
-               // new Ticket { 
+            foreach (var project in projects)
+            {
+                var count = 1;
+                foreach (var status in statuses)
+                {
+                    foreach (var type in types)
+                    {
+                        int randomNumber = random.Next(0, 2);
+                        if (randomNumber == 0)
+                        {
+                            var allSubmitters = rolesHelper.UsersInRole("Submitter").ToList();
+                            var allDevelopers = rolesHelper.UsersInRole("Developer").ToList();
+                            foreach (var priority in priorities)
+                            {
+                                int randomDays = random.Next(0, 31);
+                                var randomSubmitter = allSubmitters[random.Next(0, maxRandomSubmitter)];
+                                var randomDeveloper = allDevelopers[random.Next(0, maxRandomDeveloper)];
+                                context.Tickets.AddOrUpdate(
+                                t => t.Title,
+                                new Ticket
+                                {
+                                    ProjectId = project.Id,
+                                    TicketTypeId = type.Id,
+                                    TicketPriorityId = priority.Id,
+                                    TicketStatusId = status.Id,
+                                    SubmiterID = randomSubmitter.Id,
+                                    DeveloperID = randomDeveloper.Id,
+                                    Title = project.Name + " Demo Ticket " + count,
+                                    Discription = "A demo ticket of priority '" + priority.Name + "', type '" + type.Name + "', status '" + status.Name + "'",
+                                    Created = DateTime.Now.AddDays(-randomDays),
+                                });
+                                count++;
+                                context.SaveChanges();
+                            }
+                        }
+                    }
+                }
+                foreach (var user in rolesHelper.UsersInRole("Manager").Concat(rolesHelper.UsersInRole("Developer")).Concat(rolesHelper.UsersInRole("Submitter")))
+                {
+                    int randomNumber = random.Next(0, 2);
+                    if (randomNumber == 0)
+                    {
+                        projectsHelper.AddUserToProject(user.Id, project.Id);
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+    }
+}
 
-               // }
-
-               // );
 
             //context.TicketAttachments.AddOrUpdate(
                 //new TicketAttachment {
@@ -169,6 +232,4 @@ namespace Bug_Tracker.Migrations
                // }
 
                // );
-        }
-    }
-}
+        
